ae91551e80698fa5f15e63b4bc7c0a8d2d17d24e
Jaeeun98 <wodms4525@naver.com>
2021-05-11 17:50:13 +0900

    Title : Add
    JS100, js46, 1부터 20까지의 숫자를 일렬로 놓은 후 더하기 add

--- js44.js
+++ js44.js
@@ -1,6 +1,6 @@
 //사용자가 입력한 양의 정수의 각 자리수의 합을 구하는 프로그램을 만들어주세요
 //ex) 18234 = 1+8+2+3+4 = 18
-/*
+
 const pro = prompt('숫자를 입력해 주세요').split('');
 const num = pro.map((a) => Number(a))
 let result = 0;
@@ -10,8 +10,8 @@ for(let i=0; i<num.length; i++){
 }
 
 console.log(result);
-*/
- //원래답안
+
+/* 원래답안
 let n = prompt('숫자를 입력하세요.');
 let sum = 0;
 
@@ -21,5 +21,4 @@ while(n !== 0){
 }
 
 console.log(sum);
-
-
+*/
